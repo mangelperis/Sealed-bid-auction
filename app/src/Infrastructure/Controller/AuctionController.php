9b97b47e6e387504905ab40ba14b030a4e08fa80
mangelperis <mapi_meliana@hotmail.com>
2024-05-07 20:11:29 +0200

    Add another AuctionDemo to Controllers

--- app/src/Infrastructure/Controller/AuctionController.php
+++ app/src/Infrastructure/Controller/AuctionController.php
@@ -31,7 +31,7 @@ class AuctionController
     /**
      * @throws Exception
      */
-    #[Route('/auction/demo', name: 'index', methods: ['GET'])]
+    #[Route('/auction/task', name: 'task_auction', methods: ['GET'])]
     public function runSealedBidAuction(): Response
     {
         try {
@@ -59,6 +59,7 @@ class AuctionController
 
             $result = $this->auctionService->runAuction($reservedPrice, $bids);
 
+            //TODO Add responseHandler
             if ($result['winnerName']) {
                 return new Response(sprintf("The buyer %s wins the auction at the price of %d euros.", $result['winnerName'], $result['winningPrice']));
             }
@@ -66,6 +67,54 @@ class AuctionController
             return new Response(sprintf("There were no bids above %d euros, so no one won the auction", $reservedPrice));
         } catch (Exception $e) {
             $this->logger->error("[API] Run SealedBidAuction error: {$e->getMessage()}");
+            return new Response("Something went wrong");
+        }
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/auction/demo', name: 'demo_auction', methods: ['GET'])]
+    public function runSealedBidAuctionDemo(): Response
+    {
+        try {
+            $reservedPrice = 500;
+            $this->logger->info("Running Demo Auction for {$reservedPrice} euros!");
+
+            $buyerA = new Buyer(1, 'A');
+            $buyerB = new Buyer(2, 'B');
+            $buyerC = new Buyer(3, 'C');
+            $buyerD = new Buyer(4, 'D');
+            $buyerE = new Buyer(5, 'E');
+            //Expected Winner
+            $buyerF = new Buyer(6, 'E');
+
+            $bids = [
+                new Bid($buyerA, 400),
+                new Bid($buyerA, 499.99),
+                new Bid($buyerB, 0),
+                new Bid($buyerC, 0),
+                new Bid($buyerD, 501),
+                new Bid($buyerD, 505),
+                new Bid($buyerD, 510),
+                //Expected winningPrice
+                new Bid($buyerE, 600),
+                new Bid($buyerE, 350),
+                new Bid($buyerE, 550),
+                new Bid($buyerF, 610),
+            ];
+
+            $result = $this->auctionService->runAuction($reservedPrice, $bids);
+
+            //TODO Add responseHandler
+            if ($result['winnerName']) {
+                return new Response(sprintf("The buyer %s wins the auction at the price of %d euros.", $result['winnerName'], $result['winningPrice']));
+            }
+
+            return new Response(sprintf("There were no bids above %d euros, so no one won the auction", $reservedPrice));
+        } catch (Exception $e) {
+            $this->logger->error("[API] Run SealedBidAuctionDemo error: {$e->getMessage()}");
             return new Response("Something went wrong");
         }
 
